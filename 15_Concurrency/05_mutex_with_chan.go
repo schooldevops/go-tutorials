a35509fdec4539e4bcd01287d283b0d8b5b3f839
kido <baekido@gmail.com>
2022-06-10 13:13:52 +0900

    add mutex

---
+++ 15_Concurrency/05_mutex_with_chan.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var x = 0
+
+func increment(wg *sync.WaitGroup, ch chan bool) {
+	ch <- true
+	x = x + 1
+	<-ch
+	wg.Done()
+}
+
+func main() {
+	var w sync.WaitGroup
+	ch := make(chan bool, 1)
+	for i := 0; i < 1000; i++ {
+		w.Add(1)
+		go increment(&w, ch)
+	}
+	w.Wait()
+	fmt.Println("final value of x", x)
+}
